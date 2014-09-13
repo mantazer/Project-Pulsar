60d73d17457966215abd96bc0243ebba0c49a8c9
vrk7bp <vrk7bp@virginia.edu>
2014-09-13 01:57:02 -0400

    Configuring with Monty

--- isp-api/TwilioServer.py
+++ isp-api/TwilioServer.py
@@ -2,9 +2,11 @@ from flask import Flask, request, redirect
 import twilio.twiml
 from twilio.rest import TwilioRestClient
 import random
+import urllib2
  
 app = Flask(__name__)
- 
+
+
 @app.route("/SendRequest", methods=['GET', 'POST'])
 def start_process():
 	account_sid = "ACe36f8844f05de80021faa460764b6d33"
@@ -14,17 +16,16 @@ def start_process():
 	# email = request.form['emailAddress']
  #    address = request.form['address']
  #    number = request.form['phoneNumber']
+
+	# message = client.messages.create(body="OUT",
+	#     to="+14342008920",    # Replace with your phone number
+	#     from_=str(number)) # Replace with your Twilio number
 	 
 	message = client.messages.create(body="OUT",
 	    to="+14342008920",    # Replace with your phone number
 	    from_="+15033964667") # Replace with your Twilio number
 
-	# message = client.messages.create(body="OUT",
-	#     to="+14342008920",    # Replace with your phone number
-	#     from_=str(number)) # Replace with your Twilio number
-
 	print message.sid
-
 	return "Hello World"
 
 @app.route("/ISPFault", methods=['GET', 'POST'])
@@ -52,9 +53,31 @@ def recieve_result():
     print "Body: " + bodyValue
     print "From: " + fromValue
     print "To: " + toValue
-    
-    resp.message("")
-    return str(resp)
+
+    value = False
+
+    if(str(bodyValue) == "An outage was reported in your area. We expect this to be resolved by 6pm today."):
+    	value = True
+
+    payload = {'ispOutage': value}
+    request = requests.post("http://ec2-54-165-202-14.compute-1.amazonaws.com:5000/isp_reply", params=payload)
+    return value
+
+@app.route("/isp_reply", methods=['GET', 'POST'])
+def show_result():
+    resp = twilio.twiml.Response()
+    ispOutage = request.form['ispOutage']
+
+    if(ispOutage):
+    	print "This is working"
+    else:
+    	print "This is working (2)"
+
+    print "Outage: " + str(ispOutage)
+
+    return ispOutage
+
+#http://ec2-54-165-202-14.compute-1.amazonaws.com:5000/isp_reply
  
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
