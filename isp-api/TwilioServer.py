daba2b8ae960ca12a56af140d508ed5fa88d7c83
vrk7bp <vrk7bp@virginia.edu>
2014-09-13 02:59:10 -0400

    Please work

--- isp-api/TwilioServer.py
+++ isp-api/TwilioServer.py
@@ -68,7 +68,7 @@ def recieve_result():
 
 	payload = {'ispOutage': value}
 
-	#r = requests.post("http://ec2-54-165-202-14.compute-1.amazonaws.com:5000/isp_reply", data=payload)
+	r = requests.post("http://ec2-54-165-202-14.compute-1.amazonaws.com:5000/isp_reply", data=payload)
 
 	return "Test"
 
