1b00ac1107e28d89693eebba416278faee76e57f
Muntaser Ahmed <muntaser.r.ahmed@gmail.com>
2014-09-12 23:56:02 -0400

    added beacon model

---
+++ pulsar-beacon/src/model.py
@@ -0,0 +1,8 @@
+__author__ = 'muntaserahmed'
+
+class Beacon:
+    def __init__(self, h_address, e_address, phone):
+         self.h_address = h_address
+         self.e_address = e_address
+         self.phone = phone
+    
