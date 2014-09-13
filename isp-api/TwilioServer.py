457895ec93a262e5873c23326bb7fb3557489e2c
vrk7bp <vrk7bp@virginia.edu>
2014-09-13 03:00:09 -0400

    Please work

--- isp-api/TwilioServer.py
+++ isp-api/TwilioServer.py
@@ -66,7 +66,7 @@ def recieve_result():
 	if(str(bodyValue) == "An outage was reported in your area. We expect this to be resolved by 6pm today."):
 		value = "True"
 
-	payload = {'ispOutage': value}
+	payload = json.dumps({'ispOutage': value})
 
 	r = requests.post("http://ec2-54-165-202-14.compute-1.amazonaws.com:5000/isp_reply", data=payload)
 
