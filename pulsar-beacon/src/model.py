e181a947e534a5ffd6c3763b4cef9706dc1e2c53
Muntaser Ahmed <muntaser.r.ahmed@gmail.com>
2014-09-13 11:13:25 -0400

    updated model and prompt

--- pulsar-beacon/src/model.py
+++ pulsar-beacon/src/model.py
@@ -10,12 +10,6 @@ class Beacon:
         self.phone = '+15033964667' # testing purposes
 
     def jsonify_beacon(self):
-        payload = {'h_address': self.h_address, 'e_address': self.e_address, 'phone': self.phone}
+        payload = {'address': self.h_address, 'emailAddress': self.e_address, 'phoneNumber': self.phone}
         return payload
-    
-def prompt():
-    h_address = raw_input('Enter home address (ex: 123 1st Street, Charlottesville, VA 22903): ')
-    e_address = raw_input('Enter email address: ')
-    phone = raw_input('Enter phone number (ex: +14341234567): ')
-    return Beacon(h_address, e_address, phone)
 
