7e37461040870cd5a4cc0fe2298d3bb257b2a0bd
Muntaser Ahmed <muntaser.r.ahmed@gmail.com>
2014-09-13 09:44:25 -0400

    added backend model, mongo object, insertion

--- pulsar-backend/src/server.py
+++ pulsar-backend/src/server.py
@@ -1,14 +1,29 @@
+from db import MongoDB
 from flask import Flask, request, Response
+from model import Beacon
 import json
 import pdb
 
 app = Flask(__name__)
+mongodb = MongoDB()
 
 @app.route('/register', methods=['POST'])
 def register():
     if request.method == 'POST':
         payload = request.form
-        return Response(response=json.dumps({}), status=200)         
+        #TODO: make sure all data comes in
+         
+        h_address = payload.get('h_address')
+        e_address = payload.get('e_address')
+        phone = payload.get('phone')
+        
+        b = Beacon(h_address, e_address, phone)
+        beacon_id = mongodb.add_if_not_exists(b)
+
+        if beacon_id is not None:
+            return Response(response=json.dumps({}), status=201)
+        print 'db insertion failed'
+        return Response(response=json.dumps({}), status=500)
 
 @app.route('/listen')
 def listen():
