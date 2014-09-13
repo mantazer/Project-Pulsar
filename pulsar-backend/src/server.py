e14df8ace3d6afb631e3a62b7161f70d8a5fa552
Muntaser Ahmed <muntaser.r.ahmed@gmail.com>
2014-09-13 02:04:33 -0400

    registration

--- pulsar-backend/src/server.py
+++ pulsar-backend/src/server.py
@@ -1,14 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/register')
+@app.route('/register', methods=['POST'])
 def register():
-    return 'registering...'
+    if request.method == 'POST':
+       print request.data
 
 @app.route('/listen')
 def listen():
+    # hit /SendRequest
+    # respone key ispOutage
     return 'listening...'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
--- pulsar-beacon/src/model.py
+++ pulsar-beacon/src/model.py
@@ -2,7 +2,17 @@ __author__ = 'muntaserahmed'
 
 class Beacon:
     def __init__(self, h_address, e_address, phone):
-         self.h_address = h_address
-         self.e_address = e_address
-         self.phone = phone
+        self.h_address = h_address
+        self.e_address = e_address
+        self.phone = '+15033964667' # testing purposes
+
+    def jsonify_beacon(self):
+        payload = {'address': h_address, 'emailAddress': e_address, 'phoneNumber': phone}
+        return payload
     
+def prompt():
+    h_address = raw_input('Enter home address (Street Address, City, State Zip: ')
+    e_address = raw_input('Enter email address: ')
+    phone = raw_input('Enter phone number (+1XXXXXXXXXX): ')
+    return Beacon(h_address, e_address, phone)
+
