37e68d8d7a97d49f885547b680b6fa7f08dee2fa
vrk7bp <vrk7bp@virginia.edu>
2014-09-13 09:47:57 -0400

    Adding SendGrid

--- isp-api/TwilioServer.py
+++ isp-api/TwilioServer.py
@@ -104,7 +104,27 @@ def show_result():
 
     return str(ispOutage)
 
-#http://ec2-54-165-202-14.compute-1.amazonaws.com:5000/isp_reply
+@app.route("/sendgrid", methods=['GET', 'POST'])
+def sendgrid():
+	# Required response to SendGrid.com’s Parse API
+	print "HTTP/1.1 200 OK"
+
+	# Consume the entire email
+	envelope = simplejson.loads(request.form.get(‘envelope’))
+
+	# Get some header information
+	to_address = envelope['to'][0]
+	from_address = envelope['from']
+
+	print to_address
+	print from_address
+
+	value = "True"
+
+	payload = {'ispOutage': value}
+	r = requests.post("http://ec2-54-164-3-245.compute-1.amazonaws.com:5000/isp_reply", data=payload)
+
+	return value
  
 if __name__ == "__main__":
     app.run(host="0.0.0.0", threaded=True)
