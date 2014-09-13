402aad717d83ee7fb7a4ffe3c2372c4fce0484da
vrk7bp <vrk7bp@virginia.edu>
2014-09-13 01:40:48 -0400

    Added two number communication to server

--- isp-api/TwilioServer.py
+++ isp-api/TwilioServer.py
@@ -1,48 +1,60 @@
 from flask import Flask, request, redirect
 import twilio.twiml
 from twilio.rest import TwilioRestClient
+import random
  
 app = Flask(__name__)
  
-@app.route("/", methods=['GET', 'POST'])
-def hello_monkey():
-    """Respond to incoming calls with a simple text message."""
- 
-    resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
-    return str(resp)
+@app.route("/SendRequest", methods=['GET', 'POST'])
+def start_process():
+	account_sid = "ACe36f8844f05de80021faa460764b6d33"
+	auth_token  = "f22d67391209d2a4f8f54266cd721978"
+	client = TwilioRestClient(account_sid, auth_token)
+
+	# email = request.form['emailAddress']
+ #    address = request.form['address']
+ #    number = request.form['phoneNumber']
+	 
+	message = client.messages.create(body="OUT",
+	    to="+14342008920",    # Replace with your phone number
+	    from_="+15033964667") # Replace with your Twilio number
+
+	# message = client.messages.create(body="OUT",
+	#     to="+14342008920",    # Replace with your phone number
+	#     from_=str(number)) # Replace with your Twilio number
+
+	print message.sid
+
+	return "Hello World"
 
 @app.route("/ISPFault", methods=['GET', 'POST'])
 def test_bench_ISP():
-    """Respond to incoming calls with a simple text message."""
- 
     resp = twilio.twiml.Response()
     fromValue = request.form['From']
     bodyValue = request.form['Body']
+    toValue = request.form['To']
     if(str(bodyValue) == "OUT"):
-    	resp.message("An outage was reported in your area. We expect this to be resolved by 6pm today.")
+    	outOrNot = random.randint(0,9)
+    	if(outOrNot <= 4):
+    		resp.message("An outage was reported in your area. We expect this to be resolved by 6pm today.")
+    	else:
+    		resp.message("We are not currently aware of a service outage in your area. If you are having trouble with your service, please call 1-800-COMCAST.")
     else:
     	resp.message("We are not currently aware of a service outage in your area. If you are having trouble with your service, please call 1-800-COMCAST.")
     return str(resp)
 
-# @app.route("/NotISPFault", methods=['GET', 'POST'])
-# def test_bench_ISP():
-#     """Respond to incoming calls with a simple text message."""
- 
-#     resp = twilio.twiml.Response()
-#     resp.message("An outage was reported in your area. We expect this to be resolved by 6pm today.")
-#     return str(resp)
-
-def sendTwilioSMS():
-	# CHANGE THIS FROM TEST
-	account_sid = "ACe36f8844f05de80021faa460764b6d33"
-	auth_token  = "f22d67391209d2a4f8f54266cd721978"
-	client = TwilioRestClient(account_sid, auth_token)
-	 
-	message = client.messages.create(body="OUT",
-	    to="266278",    # Replace with your phone number
-	    from_="+15033964667") # Replace with your Twilio number
-	print message.sid
+@app.route("/RecieveResult", methods=['GET', 'POST'])
+def recieve_result():
+    resp = twilio.twiml.Response()
+    fromValue = request.form['From']
+    bodyValue = request.form['Body']
+    toValue = request.form['To']
+    print "Body: " + bodyValue
+    print "From: " + fromValue
+    print "To: " + toValue
+    
+    resp.message("")
+    return str(resp)
  
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
