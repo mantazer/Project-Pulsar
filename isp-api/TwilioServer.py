fd70136bd3db63ba5305bcd1eddd1ae50a12b1e0
vrk7bp <vrk7bp@virginia.edu>
2014-09-13 11:05:47 -0400

    Adding SendGrid

--- isp-api/TwilioServer.py
+++ isp-api/TwilioServer.py
@@ -36,15 +36,15 @@ def start_process():
 
 	#sending e-mail
 	htmlForEmail = '<html><body><img src=\"http://wedte.com/wp-content/uploads/2013/01/PowerOutage.jpg\" alt=\"Power Outage\"><p></p><p></p><h3> We think that your house may have a power outage. If this is true, simply reply to this e-mail with any response so that the Electricty Supplier can serve you faster. <p></p><br><br></h3></body></html>'
-	sg = sendgrid.SendGridClient(SendGridUserName, SendGridPassword)
+	#sg = sendgrid.SendGridClient(SendGridUserName, SendGridPassword)
 
-	# message = sendgrid.Mail()
-	# message.add_to('Gautam <raju@email.virginia.edu>')
-	# # message.add_to(email)
-	# message.set_subject('Is there a Power Outage at your house?')
-	# message.set_html(htmlForEmail)
-	# message.set_from(FromEmail)
-	# status, msg = sg.send(message)
+	message = sendgrid.Mail()
+	message.add_to('Gautam <raju@email.virginia.edu>')
+	# message.add_to(email)
+	message.set_subject('Is there a Power Outage at your house?')
+	message.set_html(htmlForEmail)
+	message.set_from(FromEmail)
+	#status, msg = sg.send(message)
 
 	#print message.sid
 	return "Hello World"
